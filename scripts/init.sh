f679d5352ef09cebfcf0c46fc3f9ce16b5f3bf7d
Cevaris <cevaris@gmail.com>
2013-12-03 11:52:48 -0700

    got script to setup RVM environment, installs gems, install chef file

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 VAGRANT=$ROOT_DIR/vagrant
 
-rvm gemset create vagrant-cassandra-dev
 
-# Install gemset file
+# Setup RVM
+RUBY_GEMSET=$ROOT_DIR/.ruby-gemset
+RUBY_VERSION=$ROOT_DIR/.ruby-version
+rvm gemset create $(cat $RUBY_GEMSET)
+rvm use "$(cat $RUBY_VERSION)@$(cat $RUBY_GEMSET)"
+
+
+# Install Gemfile
 bundle install
+
 
 # Install chef libraries
 cd $VAGRANT
